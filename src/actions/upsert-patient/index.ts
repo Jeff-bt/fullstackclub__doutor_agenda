b62f43e881098cbc833559e43e7fe253a2bda612
Jeff <jeffjunio.bt@gmail.com>
2025-06-03 20:15:22 -0300

    fix: imports organized

--- src/actions/upsert-patient/index.ts
+++ src/actions/upsert-patient/index.ts
@@ -1,6 +1,5 @@
 "use server";
 
-import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 
--- src/app/(protected)/patients/_components/upsert-patient-form.tsx
+++ src/app/(protected)/patients/_components/upsert-patient-form.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useAction } from "next-safe-action/hooks";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { NumericFormat, PatternFormat } from "react-number-format";
+import { PatternFormat } from "react-number-format";
 import { toast } from "sonner";
 import { z } from "zod";
 
